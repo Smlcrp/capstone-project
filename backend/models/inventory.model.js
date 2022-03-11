c39fe992a61c9ef070a8e2b8875a1d18d9d48909
Smlcrp <smlcrp@gmail.com>
2022-03-11 00:23:26 -0500

    added user model

--- backend/models/inventory.model.js
+++ backend/models/inventory.model.js
@@ -1,6 +1,11 @@
 const mongoose = require('mongoose');
 
 const inventorySchema = new mongoose.Schema({
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        required: true,
+        ref: 'User'
+    },
     name: {
         type: String,
         required: [true, 'please enter a name for the inventory item']
