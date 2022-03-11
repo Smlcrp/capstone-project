8d2a48e703d727ecba15a4165cbf7f19dda72738
Smlcrp <smlcrp@gmail.com>
2022-03-10 21:34:48 -0500

    created routes folder, started initial inventory routes

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,10 @@
-console.log('hello world')
+const express = require('express');
+const dotenv = require('dotenv').config();
+const port = process.env.PORT || 5000
+
+const app = express()
+
+app.use('/api/inventory', require('./routes/inventory.routes'));
+
+
+app.listen(port, () => console.log(`Server started on port ${port}`))
