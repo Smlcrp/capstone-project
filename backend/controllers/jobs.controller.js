50ce0dd81fe42517e2639e1eece1697de1efed48
Smlcrp <smlcrp@gmail.com>
2022-03-13 16:53:16 -0400

    small backend fix, toast container makes web page go blank

--- backend/controllers/jobs.controller.js
+++ backend/controllers/jobs.controller.js
@@ -48,16 +48,16 @@ const updateJobs = asyncHandler(async (req, res) => {
         throw new Error('Job not found')
     }
 
-    const user = await User.findById(req.user.id)
+    
 
     // Check for user
-    if(!user) {
+    if(!req.user) {
         res.status(401)
         throw new Error('User not found')
     }
 
     // Make sure logged in user matches job user
-    if(job.user.toString() !== user.id) {
+    if(job.user.toString() !== req.user.id) {
         res.status(401)
         throw new Error('User not authorized')
     }
@@ -77,16 +77,14 @@ const deleteJobs = asyncHandler(async (req, res) => {
         throw new Error('Job not found')
     }
 
-    const user = await User.findById(req.user.id)
-
     // Check for user
-    if(!user) {
+    if(!req.user) {
         res.status(401)
         throw new Error('User not found')
     }
 
     // Make sure logged in user matches job user
-    if(job.user.toString() !== user.id) {
+    if(job.user.toString() !== req.user.id) {
         res.status(401)
         throw new Error('User not authorized')
     }
