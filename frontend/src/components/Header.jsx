a69bc5454b1844f342bd0baca7df6d2385cb8b37
Smlcrp <smlcrp@gmail.com>
2022-03-13 16:48:35 -0400

    register, login, and logout all work with state tracking

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,23 +1,45 @@
 import {FaSignInAlt, FaSignOutAlt, FaUser, FaUserPlus} from 'react-icons/fa'
-import {Link} from 'react-router-dom'
+import {Link, useNavigate} from 'react-router-dom'
+import {useSelector, useDispatch} from 'react-redux'
+import {logout, reset} from '../features/auth/auth.slice'
+
 
 function Header() {
+    const navigate = useNavigate()
+    const dispatch = useDispatch()
+    const {user} = useSelector((state) => state.auth)
+
+    const onLogout = () => {
+        dispatch(logout())
+        dispatch(reset())
+        navigate('/')
+
+    }
+
   return (
     <header className='header'>
         <div className="logo">
             <Link to='/'>Mockey Wrench</Link>
         </div>
         <ul>
-            <li>
-                <Link to='/login'>
-                    <FaSignInAlt/> Login
-                </Link>
-            </li>
-            <li>
-                <Link to='/register'>
-                    <FaUserPlus/> Register
-                </Link>
-            </li>
+            {user ? (
+                <li>
+                    <button className='btn' onClick={onLogout}>
+                        <FaSignInAlt/> Logout
+                    </button>
+                </li>) : (<>
+                <li>
+                    <Link to='/login'>
+                        <FaSignInAlt/> Login
+                    </Link>
+                </li>
+                <li>
+                    <Link to='/register'>
+                        <FaUserPlus/> Register
+                    </Link>
+                </li>
+            </>)}
+            
         </ul>
     </header>
   )
--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,5 +1,10 @@
 import {useState, useEffect} from 'react';
 import {FaUserPlus} from 'react-icons/fa';
+import {useSelector, useDispatch} from 'react-redux';
+import {useNavigate} from 'react-router-dom';
+import {toast} from 'react-toastify';
+import {register, reset} from '../features/auth/auth.slice';
+import Spinner from '../components/Spinner';
 
 function Register() {
     const [formData, setFormData] = useState({
@@ -11,6 +16,24 @@ function Register() {
 
     const { name, email, password, password2 } = formData
 
+    const navigate = useNavigate()
+    const dispatch = useDispatch()
+
+    const {user, isLoading, isError, isSuccess, message} = useSelector((state) => state.auth)
+
+    useEffect(() => {
+        if(isError) {
+            toast.error(message)
+        }
+
+        if(isSuccess || user) {
+            navigate('/')
+        }
+
+        dispatch(reset())
+
+    }, [user, isError, isSuccess, message, navigate, dispatch])
+
     const onChange = (e) => {
         setFormData((prevState) => ({
             ...prevState,
@@ -20,64 +43,83 @@ function Register() {
 
     const onSubmit = (e) => {
         e.preventDefault()
+
+        if(password !== password2) {
+            toast.error('Passwords do not match')
+        } else {
+           const userData = {
+               name,
+               email,
+               password,
+           } 
+
+           dispatch(register(userData))
+        }
     }
 
 
-  return <>
-    <section className= 'heading'>
-        <h1>
-            <FaUserPlus/> Register
-        </h1>
-        <p>Please Create An Account</p>
-    </section>
-    <section className="form">
-        <form onSubmit={onSubmit}>
-            <div className="form-group">
-                <input 
-                type='text' 
-                className="form-control" 
-                id='name' name='name' 
-                value={name} 
-                placeholder='Please enter your name' 
-                onChange={onChange} 
-                />
-            </div>
-            <div className="form-group">
-                <input 
-                type='email' 
-                className="form-control" 
-                id='email' name='email' 
-                value={email} 
-                placeholder='Please enter your email' 
-                onChange={onChange} 
-                />
-            </div>
-            <div className="form-group">
-                <input 
-                type='password' 
-                className="form-control" 
-                id='password' name='password' 
-                value={password} 
-                placeholder='Please enter a password' 
-                onChange={onChange} 
-                />
-            </div>
-            <div className="form-group">
-                <input 
-                type='password' 
-                className="form-control" 
-                id='password2' name='password2' 
-                value={password2} 
-                placeholder='Please confirm your password' 
-                onChange={onChange} 
-                />
-            </div>
-            <div className="form-group">
-                <button type='submit' className='btn btn-block'>Submit</button>
-            </div>
-        </form>  
-    </section>
-  </>
+    if(isLoading) {
+        return <Spinner/>
+    }
+
+  return (
+    <>
+        <section className= 'heading'>
+            <h1>
+                <FaUserPlus/> Register
+            </h1>
+            <p>Please Create An Account</p>
+        </section>
+        <section className="form">
+            <form onSubmit={onSubmit}>
+                <div className="form-group">
+                    <input 
+                    type='text' 
+                    className="form-control" 
+                    id='name' 
+                    name='name' 
+                    value={name} 
+                    placeholder='Please enter your name' 
+                    onChange={onChange} 
+                    />
+                </div>
+                <div className="form-group">
+                    <input 
+                    type='email' 
+                    className="form-control" 
+                    id='email' name='email' 
+                    value={email} 
+                    placeholder='Please enter your email' 
+                    onChange={onChange} 
+                    />
+                </div>
+                <div className="form-group">
+                    <input 
+                    type='password' 
+                    className="form-control" 
+                    id='password' name='password' 
+                    value={password} 
+                    placeholder='Please enter a password' 
+                    onChange={onChange} 
+                    />
+                </div>
+                <div className="form-group">
+                    <input 
+                    type='password' 
+                    className="form-control" 
+                    id='password2' name='password2' 
+                    value={password2} 
+                    placeholder='Please confirm your password' 
+                    onChange={onChange} 
+                    />
+                </div>
+                <div className="form-group">
+                    <button type='submit' className='btn btn-block'>Submit</button>
+                </div>
+            </form>  
+        </section>
+    </>
+  )
 }
 
 export default Register;
